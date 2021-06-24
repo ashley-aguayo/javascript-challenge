a137119aaed239b48aa638dccd3fb36cfee98805
ashley-aguayo <ashley.aguayo@outlook.com>
2021-06-23 17:05:20 -0700

    update

--- static/js/app.js
+++ static/js/app.js
@@ -5,6 +5,8 @@ console.log(tableData);
 //creat reference for table body
 var tbody = d3.select("tbody");
 console.log(tbody);
+//var inputDate = d3.select(#datetime);
+//var inputCity = d3.select(#city);
 
 //loop through data
 data.forEach(function(alienData) {
@@ -19,15 +21,25 @@ data.forEach(function(alienData) {
     Object.entries(alienData).forEach(function([key, value]) {
     console.log(key, value);
         //Append a cell to the row
-        var cell = row.append("td");
+        var cell = tbody.append("td");
+        cell.text(value);
     });
 });
 
 //select buttons and form
 var button1 = d3.select("#filter-btn");
+var inputField = d3.select('#input-field');
 var form = d3.select("form");
 
 //create event handlers
 button1.on("click", runEnter);
 form.on("click",runEnter);
 
+//complete the event handler function form
+function runEnter() {
+    d3.event.preventDefault();
+
+    var inputElement = d3.select(".form-control");
+    var inputValue = inputElement.property("value");
+    var results = tableData.filter(data => data.datetime);
+};
